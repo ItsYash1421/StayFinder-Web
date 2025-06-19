810124ab83eadb28fbc491603f31939ab6daee9c
Yash <118480441+ItsYash1421@users.noreply.github.com>
2025-06-19 18:37:14 +0530

    Update listings.ts

--- frontend/src/api/listings.ts
+++ frontend/src/api/listings.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8000/api/listings"; // adjust as needed
+const API_URL = "https://stayfinder-backend-5wmn.onrender.com"; // adjust as needed
 
 export const fetchListings = async () => {
   const { data } = await axios.get(API_URL);
