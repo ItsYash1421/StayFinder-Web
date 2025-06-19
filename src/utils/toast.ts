49bba8e83bc508b71928d6fb741cc7c0f907b423
Yash <118480441+ItsYash1421@users.noreply.github.com>
2025-06-19 17:38:56 +0530

    Delete

--- src/utils/toast.ts
+++
@@ -1,7 +0,0 @@
-type ToastType = 'success' | 'error' | 'info' | 'warning';
-
-export const showToast = (message: string, type: ToastType = 'info') => {
-  // For now, we'll just use console.log
-  // In a real app, you would use a toast library like react-toastify
-  console.log(`[${type.toUpperCase()}] ${message}`);
-}; 
